3bcbad0e0a455f9e71110143c9abdbe23aedaa25
Jacob Ilagan <jilagan5@myseneca.ca>
2025-01-30 14:58:31 -0500

    Begin writing tests

---
+++ tests/unit/memory.test.js
@@ -0,0 +1,9 @@
+const MemoryDB = require('../../src/model/data/memory/index');
+import {
+  listFragments,
+  writeFragment,
+  readFragment,
+  writeFragmentData,
+  readFragmentData,
+  deleteFragment,
+} from '../../src/model/data/memory/index';
