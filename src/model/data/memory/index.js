285f6f04ba5a97aa5480052dc072c107b0108978
Jacob Ilagan <jilagan5@myseneca.ca>
2025-01-30 14:01:04 -0500

    Functions for metadata and data parsing

--- src/model/data/memory/index.js
+++ src/model/data/memory/index.js
@@ -3,3 +3,19 @@ const MemoryDB = require('./memory-db');
 // Create two in-memory databases: one for fragment metadata and the other for raw data
 const data = new MemoryDB();
 const metadata = new MemoryDB();
+
+// Write a fragment's metadata to memory db. Returns a Promise<void>
+function writeFragment(fragment) {
+  // Simulate db/network serialization of the value, storing only JSON representation.
+  // This is important because it's how things will work later with AWS data stores
+  const serialized = JSON.stringify(fragment);
+  return metadata.put(fragment.ownerId, fragment.id, serialized);
+}
+
+// Read a fragment's metadata from memory db. Returns a Promise<Object>
+async function readFragment(ownerId, id) {
+  // NOTE: this data will be raw JSON, we need to turn it back into an Object.
+  // You'll need to take care of converting this back into a Fragment instance higher up in the callstack
+  const serialized = await metadata.get(ownerId, id);
+  return typeof serialized === 'string' ? JSON.parse(serialized) : serialized;
+}
