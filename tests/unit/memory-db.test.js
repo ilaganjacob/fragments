48c482396df4d32349fddb6ab68f9f4551427132
Jacob Ilagan <jilagan5@myseneca.ca>
2025-01-30 13:25:04 -0500

    Implement tests for memory-db.js

--- tests/unit/memory-db.test.js
+++ tests/unit/memory-db.test.js
@@ -1,1 +1,14 @@
 const MemoryDB = require('../../src/model/data/memory/memory-db');
+
+describe('memory-db', () => {
+  let db;
+  // Each test will get its own, empty database instance
+  beforeEach(() => {
+    db = new MemoryDB();
+  });
+
+  test('put() returns nothing', async () => {
+    const result = await db.put('a', 'b', {});
+    expect(result).toBe(undefined);
+  });
+});
