fe64b537f7a86efbe86c891cf16f9c92bc252a15
Jacob Ilagan <jilagan5@myseneca.ca>
2025-01-30 14:11:30 -0500

    Fixed formatting

--- src/model/data/memory/index.js
+++ src/model/data/memory/index.js
@@ -7,7 +7,7 @@ const metadata = new MemoryDB();
 // Write a fragment's metadata to memory db. Returns a Promise<void>
 function writeFragment(fragment) {
   // Simulate db/network serialization of the value, storing only JSON representation.
-  // This is important because it's how things will work later with AWS data stores
+  // This is important because it's how things will work later with AWS data stores.
   const serialized = JSON.stringify(fragment);
   return metadata.put(fragment.ownerId, fragment.id, serialized);
 }
@@ -15,7 +15,8 @@ function writeFragment(fragment) {
 // Read a fragment's metadata from memory db. Returns a Promise<Object>
 async function readFragment(ownerId, id) {
   // NOTE: this data will be raw JSON, we need to turn it back into an Object.
-  // You'll need to take care of converting this back into a Fragment instance higher up in the callstack
+  // You'll need to take care of converting this back into a Fragment instance
+  // higher up in the callstack.
   const serialized = await metadata.get(ownerId, id);
   return typeof serialized === 'string' ? JSON.parse(serialized) : serialized;
 }
@@ -49,7 +50,7 @@ function deleteFragment(ownerId, id) {
   return Promise.all([
     // Delete metadata
     metadata.del(ownerId, id),
-    // Delete the data
+    // Delete data
     data.del(ownerId, id),
   ]);
 }
