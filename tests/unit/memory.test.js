1cb85e5f8eb790eb761e0f7aeefa69c3b6ffb9ee
Jacob Ilagan <jilagan5@myseneca.ca>
2025-02-02 18:19:11 -0500

    Reformat tests grouping

--- tests/unit/memory.test.js
+++ tests/unit/memory.test.js
@@ -6,7 +6,9 @@ const {
 } = require('../../src/model/data/memory/index');
 
 // readFragment test
-describe('Fragment Memory Database tests', () => {
+
+const testData = Buffer.from('Hello Test');
+describe('Fragment Metadata tests ', () => {
   const ownerId = '1234';
   const fragmentId = 'abcd';
   const testFragment = {
@@ -15,67 +17,63 @@ describe('Fragment Memory Database tests', () => {
     type: 'text/plain',
     size: 10,
   };
+  test('writeFragment should store fragment metadata', async () => {
+    // await because it returns a promise
+    await writeFragment(testFragment);
 
-  const testData = Buffer.from('Hello Test');
-  describe('Fragment Metadata tests ', () => {
-    test('writeFragment should store fragment metadata', async () => {
-      // await because it returns a promise
-      await writeFragment(testFragment);
-
-      const result = await readFragment(ownerId, fragmentId);
-      expect(result).toEqual(testFragment);
-    });
-
-    test('should return undefined for non-existent fragment', async () => {
-      const result = await readFragment(ownerId, 'sss');
-      expect(result).toBe(undefined);
-    });
-
-    test('writeFragment should throw for invalid fragment id', async () => {
-      expect.assertions(1); // Ensure the expect in the catch block is called since checking for error
-
-      const invalidFragment = {
-        ...testFragment,
-        id: null,
-      };
-
-      try {
-        await writeFragment(invalidFragment);
-      } catch (error) {
-        expect(error.message).toBe(
-          'primaryKey and secondaryKey strings are required, got primaryKey=1234, secondaryKey=null'
-        );
-      }
-    });
-
-    test('writeFragment should throw for invalid ownerId', async () => {
-      expect.assertions(1); // Ensure the expect in the catch block is called since checking for error
-
-      const invalidFragment = {
-        ...testFragment,
-        ownerId: null,
-      };
-
-      try {
-        await writeFragment(invalidFragment);
-      } catch (error) {
-        expect(error.message).toBe(
-          'primaryKey and secondaryKey strings are required, got primaryKey=null, secondaryKey=abcd'
-        );
-      }
-    });
+    const result = await readFragment(ownerId, fragmentId);
+    expect(result).toEqual(testFragment);
   });
 
-  describe('Fragment Data tests', () => {
-    test('writeFragmentData should store and read Buffer data', async () => {
-      await writeFragmentData(ownerId, fragmentId, testData);
-      const result = await readFragmentData(ownerId, fragmentId);
-      expect(Buffer.compare(result, testData)).toBe(0);
-    });
+  test('should return undefined for non-existent fragment', async () => {
+    const result = await readFragment(ownerId, 'sss');
+    expect(result).toBe(undefined);
+  });
 
-    test('readFragmentData should return undefined for non-existent data', async () => {
-      const result = await readFragmentData(ownerId, 'doesnotexist');
-      expect(result).toBe(undefined);
-    });
+  test('writeFragment should throw for invalid fragment id', async () => {
+    expect.assertions(1); // Ensure the expect in the catch block is called since checking for error
+
+    const invalidFragment = {
+      ...testFragment,
+      id: null,
+    };
+
+    try {
+      await writeFragment(invalidFragment);
+    } catch (error) {
+      expect(error.message).toBe(
+        'primaryKey and secondaryKey strings are required, got primaryKey=1234, secondaryKey=null'
+      );
+    }
+  });
+
+  test('writeFragment should throw for invalid ownerId', async () => {
+    expect.assertions(1); // Ensure the expect in the catch block is called since checking for error
+
+    const invalidFragment = {
+      ...testFragment,
+      ownerId: null,
+    };
+
+    try {
+      await writeFragment(invalidFragment);
+    } catch (error) {
+      expect(error.message).toBe(
+        'primaryKey and secondaryKey strings are required, got primaryKey=null, secondaryKey=abcd'
+      );
+    }
+  });
+});
+
+describe('Fragment Data tests', () => {
+  test('writeFragmentData should store and read Buffer data', async () => {
+    await writeFragmentData(ownerId, fragmentId, testData);
+    const result = await readFragmentData(ownerId, fragmentId);
+    expect(Buffer.compare(result, testData)).toBe(0);
+  });
+
+  test('readFragmentData should return undefined for non-existent data', async () => {
+    const result = await readFragmentData(ownerId, 'doesnotexist');
+    expect(result).toBe(undefined);
   });
 });
