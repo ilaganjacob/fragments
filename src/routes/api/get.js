dbd6c3645384d0f5b863d35c4947d5247dfc4d9f
Jacob Ilagan <jilagan5@myseneca.ca>
2025-02-06 16:34:44 -0500

    Remove old comments

--- src/routes/api/get.js
+++ src/routes/api/get.js
@@ -6,11 +6,4 @@ const { createSuccessResponse } = require('../../response');
 module.exports = (req, res) => {
   // TODO: this is just a placeholder. To get something working, return an empty array...
   res.status(200).json(createSuccessResponse({ fragments: [] }));
-
-  // **** OLD CODE ****
-  // res.status(200).json({
-  //   status: 'ok',
-  //   // TODO: change me
-  //   fragments: [],
-  // });
 };
