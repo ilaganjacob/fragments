72bb6482e00adc4df26a03e9c3781a3d8ee5f625
Jacob Ilagan <jilagan5@myseneca.ca>
2025-02-06 17:20:22 -0500

    Comment properly

--- src/routes/api/post.js
+++ src/routes/api/post.js
@@ -9,17 +9,28 @@ module.exports = async (req, res) => {
     return res.status(415).json(createErrorResponse(415, 'Unsupported Media Type'));
   }
 
-  // Since there can be errors during creation, put in try-catch
+  // Try to create and save the fragment
   try {
-    const contentType = req.get('content-type');
+    // Get what type of content they sent (from Content-Type header)
+    const contentType = req.get('content-type'); // e.g. "text/plain"
+
+    // owner (from auth middleware in req.user)
+    // type (from Content-Type header)
+    // size (length of the data they sent)
     const fragment = new Fragment({
       ownerId: req.user,
       type: contentType,
       size: req.body.length,
     });
 
+    // Save the fragment metadata
     await fragment.save();
+
+    // Save the actual content
     await fragment.setData(req.body);
+
+    // Create Location header URL using the new fragment's id
+    // If API_URL is in .env, use that or else create from req
 
     let fragmentsUrl;
     if (process.env.API_URL) {
@@ -27,6 +38,26 @@ module.exports = async (req, res) => {
     } else {
       fragmentsUrl = new URL(`/v1/fragments/`, `${req.protocol}://{req.headers.host}`);
     }
+
+    // This is the URL where we can GET this fragment
+    const location = new URL(fragment.id, fragmentsUrl).href;
+
+    // Return the fragment's metadata and location
+    res
+      .location(location)
+      .status(201)
+      .json(
+        createSuccessResponse({
+          fragment: {
+            id: fragment.id,
+            ownerId: fragment.ownerId,
+            created: fragment.created,
+            updated: fragment.updated,
+            type: fragment.type,
+            size: fragment.size,
+          },
+        })
+      );
   } catch (err) {
     logger.error({ err }, 'Error creating fragment');
     res.status(415).json(createErrorResponse(415, 'Unsupported Media Type'));
