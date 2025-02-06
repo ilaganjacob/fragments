d7704b90fcc2316a902e56a71823492a8796f8a3
Jacob Ilagan <jilagan5@myseneca.ca>
2025-02-06 16:43:24 -0500

    Create request validation

--- src/routes/api/post.js
+++ src/routes/api/post.js
@@ -1,3 +1,11 @@
-const { createSuccessResponse } = require('../../response');
+const { Fragment } = require('../../model/fragment');
+const { createSuccessResponse, createErrorResponse } = require('../../response');
+const logger = require('../../logger');
 
-module.exports = async (req, res) => {};
+module.exports = async (req, res) => {
+  if (!Buffer.isBuffer(req.body)) {
+    logger.warn('Request body is not a Buffer');
+    // Wrong media type code
+    return res.status(415).json(createErrorResponse(415, 'Unsupported Media Type'));
+  }
+};
