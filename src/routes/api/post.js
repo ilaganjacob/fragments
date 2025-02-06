79fc0bc24fff7b40b08e5430b209d2543483aaf0
Jacob Ilagan <jilagan5@myseneca.ca>
2025-02-06 17:10:16 -0500

    Create url from .env

--- src/routes/api/post.js
+++ src/routes/api/post.js
@@ -8,4 +8,27 @@ module.exports = async (req, res) => {
     // Wrong media type code
     return res.status(415).json(createErrorResponse(415, 'Unsupported Media Type'));
   }
+
+  // Since there can be errors during creation, put in try-catch
+  try {
+    const contentType = req.get('content-type');
+    const fragment = new Fragment({
+      ownerId: req.user,
+      type: contentType,
+      size: req.body.length,
+    });
+
+    await fragment.save();
+    await fragment.setData(req.body);
+
+    let fragmentsUrl;
+    if (process.env.API_URL) {
+      fragmentsUrl = new URL('/v1/fragments/', process.env.API_URL);
+    } else {
+      fragmentsUrl = new URL(`/v1/fragments/`, `${req.protocol}://{req.headers.host}`);
+    }
+  } catch (err) {
+    logger.error({ err }, 'Error creating fragment');
+    res.status(415).json(createErrorResponse(415, 'Unsupported Media Type'));
+  }
 };
