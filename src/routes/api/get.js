7461cadd26167e4fff254705573161a03646e08b
Jacob Ilagan <jilagan5@myseneca.ca>
2025-02-07 14:31:41 -0500

    Implement get route along with logger calls

--- src/routes/api/get.js
+++ src/routes/api/get.js
@@ -1,9 +1,35 @@
 // src/routes/api/get.js
-const { createSuccessResponse } = require('../../response');
+const { createSuccessResponse, createErrorResponse } = require('../../response');
+const { Fragment } = require('../../model/fragment');
+const logger = require('../../logger');
 /**
  * Get a list of fragments for the current user
  */
-module.exports = (req, res) => {
-  // TODO: this is just a placeholder. To get something working, return an empty array...
-  res.status(200).json(createSuccessResponse({ fragments: [] }));
+module.exports = async (req, res) => {
+  try {
+    // Debug log: Incoming request details
+    logger.debug(
+      {
+        method: req.method,
+        path: req.path,
+        user: req.user,
+      },
+      'Incoming get fragments request'
+    );
+
+    // Get the fragments for the current user
+    // 'false' means we only want fragment ids
+    // 'true' gives full fragments
+    const fragments = await Fragment.byUser(req.user, false);
+
+    // Debug log: Fragment retrieval details
+    logger.debug(
+      {
+        fragmentCount: fragments.length,
+      },
+      'Fragments received for user'
+    );
+  } catch (err) {
+    logger.error({ err }, 'Unexpected error receiving fragments');
+  }
 };
