bfa03bb63a961d4f6ad1a7624c9ccfafd8005af1
Jacob Ilagan <jilagan5@myseneca.ca>
2025-02-06 20:36:36 -0500

    Create test for unauthenticated request

--- tests/unit/post.test.js
+++ tests/unit/post.test.js
@@ -1,3 +1,10 @@
 // tests/unit/post.test.js
 const request = require('supertest');
 const app = require('../../src/app');
+
+describe('POST /v1/fragments', () => {
+  // If the request is missing the Authorization header, it should be forbidden
+  test('unauthenticated requests are denied', () => {
+    request(app).post('/v1/fragments').expect(401);
+  });
+});
