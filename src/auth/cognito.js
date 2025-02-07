ffd9027098ce2f73110dd111b53f9d913cdee2a7
Jacob Ilagan <jilagan5@myseneca.ca>
2025-02-07 17:14:07 -0500

    Remove passport from imports

--- src/auth/cognito.js
+++ src/auth/cognito.js
@@ -4,10 +4,11 @@
 // Identity Token provided by Cognito. The token will be
 // parsed from the Authorization header (i.e., Bearer Token).
 
-const passport = require('passport');
 const BearerStrategy = require('passport-http-bearer').Strategy;
 const { CognitoJwtVerifier } = require('aws-jwt-verify');
+
 const authorize = require('./auth-middleware');
+
 const logger = require('../logger');
 
 // We expect AWS_COGNITO_POOL_ID and AWS_COGNITO_CLIENT_ID to be defined.
