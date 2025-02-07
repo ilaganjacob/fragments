e0bcc49245c8feb651f1b1798182b13ca2bf9fb4
Jacob Ilagan <jilagan5@myseneca.ca>
2025-02-07 15:33:24 -0500

    Fix test for user with no fragments, expect an empty array

--- tests/unit/get.test.js
+++ tests/unit/get.test.js
@@ -13,11 +13,15 @@ describe('GET /v1/fragments', () => {
     request(app).get('/v1/fragments').auth('invalid@email.com', 'incorrect_password').expect(401));
 
   // Using a valid username/password pair should give a success result with a .fragments array
-  test('authenticated users get a fragments array', async () => {
-    const res = await request(app).get('/v1/fragments').auth('user1@email.com', 'password1');
-    expect(res.statusCode).toBe(200);
+  test('authenticated users with no fragments returns an empty array', async () => {
+    const res = await request(app)
+      .get('/v1/fragments')
+      .auth('user1@email.com', 'password1')
+      .expect(200);
+
     expect(res.body.status).toBe('ok');
     expect(Array.isArray(res.body.fragments)).toBe(true);
+    expect(res.body.fragments.length).toBe(0);
   });
 
   // TODO: we'll need to add tests to check the contents of the fragments array later
