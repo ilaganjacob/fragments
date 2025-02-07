38b342ea22946a60425f13b70f8b43b71be9589e
Jacob Ilagan <jilagan5@myseneca.ca>
2025-02-06 20:40:57 -0500

    Return the request to let Jest know the test is complete

--- tests/unit/post.test.js
+++ tests/unit/post.test.js
@@ -5,11 +5,11 @@ const app = require('../../src/app');
 describe('POST /v1/fragments', () => {
   // If the request is missing the Authorization header, it should be forbidden
   test('unauthenticated requests are denied', () => {
-    request(app).post('/v1/fragments').expect(401);
+    return request(app).post('/v1/fragments').expect(401);
   });
 
   // If the username & password do not exist, it should be forbidden
   test('nonexistent username and password combination', () => {
-    request(app).post('/v1/fragments').auth('invalid@email.com', 'invalidPass').expect(401);
+    return request(app).post('/v1/fragments').auth('invalid@email.com', 'invalidPass').expect(401);
   });
 });
