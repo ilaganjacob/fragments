f6aa07f42048ae9af6fce5c0a0b0c1da8e206d22
Jacob Ilagan <jilagan5@myseneca.ca>
2025-02-06 20:54:52 -0500

    Refactor to use async / await

--- tests/unit/post.test.js
+++ tests/unit/post.test.js
@@ -4,13 +4,13 @@ const app = require('../../src/app');
 
 describe('POST /v1/fragments', () => {
   // If the request is missing the Authorization header, it should be forbidden
-  test('unauthenticated requests are denied', () => {
-    return request(app).post('/v1/fragments').expect(401);
+  test('unauthenticated requests are denied', async () => {
+    await request(app).post('/v1/fragments').expect(401);
   });
 
   // If the username & password do not exist, it should be forbidden
-  test('nonexistent username and password combination', () => {
-    return request(app).post('/v1/fragments').auth('invalid@email.com', 'invalidPass').expect(401);
+  test('nonexistent username and password combination', async () => {
+    await request(app).post('/v1/fragments').auth('invalid@email.com', 'invalidPass').expect(401);
   });
 
   // If the content type is unsupported, reject it
