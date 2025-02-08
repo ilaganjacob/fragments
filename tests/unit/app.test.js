acc7f7dd5b58c6253dcf9b8b3c0f2c925fe096c0
Jacob Ilagan <jilagan5@myseneca.ca>
2025-02-08 15:53:26 -0500

    Fix testing

--- tests/unit/app.test.js
+++ tests/unit/app.test.js
@@ -1,9 +1,18 @@
-// test/unit/app.test.js
-
+// tests/unit/app.test.js
 const request = require('supertest');
-
 const app = require('../../src/app');
 
-describe('404 test', () => {
-  test('non-existent route attempt', () => request(app).get('/hellohi').expect(404));
+describe('App Error Handling', () => {
+  test('handles 404 for non-existent routes', async () => {
+    const res = await request(app).get('/non-existent-route');
+
+    expect(res.statusCode).toBe(404);
+    expect(res.body).toEqual({
+      status: 'error',
+      error: {
+        code: 404,
+        message: 'not found',
+      },
+    });
+  });
 });
