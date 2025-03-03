6bf1de489c94559d39457532733c018053367d0b
Jacob Ilagan <jilagan5@myseneca.ca>
2025-03-03 17:00:20 -0500

    Fix code to use CognitoJwtVerifier

--- src/auth/cognito.js
+++ src/auth/cognito.js
@@ -8,26 +8,24 @@ const BearerStrategy = require('passport-http-bearer').Strategy;
 const { CognitoJwtVerifier } = require('aws-jwt-verify');
 
 const authorize = require('./auth-middleware');
-
 const logger = require('../logger');
 
 // We expect AWS_COGNITO_POOL_ID and AWS_COGNITO_CLIENT_ID to be defined.
 if (!(process.env.AWS_COGNITO_POOL_ID && process.env.AWS_COGNITO_CLIENT_ID)) {
   throw new Error('missing expected env vars: AWS_COGNITO_POOL_ID, AWS_COGNITO_CLIENT_ID');
 }
-
-// Log that we're using Cognito
-logger.info('Using AWS Cognito for auth');
-
 // Create a Cognito JWT Verifier, which will confirm that any JWT we
 // get from a user is valid and something we can trust. See:
 // https://github.com/awslabs/aws-jwt-verify#cognitojwtverifier-verify-parameters
 const jwtVerifier = CognitoJwtVerifier.create({
+  // These variables must be set in the .env
   userPoolId: process.env.AWS_COGNITO_POOL_ID,
   clientId: process.env.AWS_COGNITO_CLIENT_ID,
   // We expect an Identity Token (vs. Access Token)
   tokenUse: 'id',
 });
+
+logger.info('Configured to use AWS Cognito for Authorization');
 
 // At startup, download and cache the public keys (JWKS) we need in order to
 // verify our Cognito JWTs, see https://auth0.com/docs/secure/tokens/json-web-tokens/json-web-key-sets
@@ -43,46 +41,25 @@ jwtVerifier
   });
 
 module.exports.strategy = () =>
+  // For our Passport authentication strategy, we'll look for the Bearer Token
+  // in the Authorization header, then verify that with our Cognito JWT Verifier.
   new BearerStrategy(async (token, done) => {
     try {
-      logger.info({
-        msg: 'Verifying Token',
-        userPoolId: process.env.AWS_COGNITO_POOL_ID,
-        clientId: process.env.AWS_COGNITO_CLIENT_ID,
-        tokenLength: token.length,
-        tokenHeader: token.split('.')[0],
-      });
-
-      // Decode token to inspect its contents
-      const decoded = require('jwt-decode')(token);
-      logger.info({
-        msg: 'Decoded Token Details',
-        sub: decoded.sub,
-        aud: decoded.aud,
-        exp: decoded.exp,
-        email: decoded.email,
-        username: decoded['cognito:username'],
-        currentTime: Math.floor(Date.now() / 1000),
-      });
-
+      // Verify this JWT
       const user = await jwtVerifier.verify(token);
+      logger.debug({ user }, 'verified user token');
 
-      logger.info({
-        msg: 'Verification Successful',
-        email: user.email,
-        username: user['cognito:username'],
-      });
-
+      // Create a user, but only bother with their email. We could
+      // also go lookup more info in a database, but we don't need it.
       done(null, user.email);
     } catch (err) {
-      logger.error({
-        msg: 'Token Verification FAILED',
-        errorName: err.name,
-        errorMessage: err.message,
-        errorStack: err.stack,
-      });
+      logger.error({ err, token }, 'could not verify token');
       done(null, false);
     }
   });
+
+// Previously we defined `authenticate()` like this:
+// module.exports.authenticate = () => passport.authenticate('bearer', { session: false });
+//
 // Now we'll delegate the authorization to our authorize middleware
 module.exports.authenticate = () => authorize('bearer');
